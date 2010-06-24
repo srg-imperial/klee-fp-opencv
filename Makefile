cda638c489431b4397be5f07e3c6b0d4bd9a76d7
Peter Collingbourne <pcc03@doc.ic.ac.uk>
2010-06-24 19:27:19 +0100

    initial

---
+++ Makefile
@@ -0,0 +1,13 @@
+LLVM_BUILD_PATH = /homes/pcc03/data/src/llvm/llvm-2.7-dbg/Debug
+LLVMGCC_PATH = /homes/pcc03/data/src/llvm/llvm-gcc4.2-2.7-x86_64-linux
+KLEE_PATH = /homes/pcc03/data/src/llvm/klee
+OPENCV_PATH = /data/pcc03/src/OpenCV-2.1.0
+OPENCV_BUILD_PATH = /data/pcc03/src/OpenCV-2.1.0-build-llvm
+
+all: eigenval.exe
+
+%.bc: %.cpp
+	$(LLVMGCC_PATH)/bin/llvm-g++ -I$(OPENCV_PATH)/include/opencv -I$(KLEE_PATH)/include/klee -I$(OPENCV_BUILD_PATH) -c -emit-llvm eigenval.cpp -o $@
+
+eigenval.exe: eigenval.bc
+	$(LLVM_BUILD_PATH)/bin/llvm-ld -disable-opt $^ $(OPENCV_BUILD_PATH)/lib/libcv.a $(OPENCV_BUILD_PATH)/lib/libcxcore.a -o eigenval.exe
---
+++ eigenval.cpp
@@ -0,0 +1,17 @@
+#include "cv.h"
+#include <klee.h>
+
+int main(void) {
+	IplImage *img1 = cvCreateImage(cvSize(4, 4), IPL_DEPTH_32F, 1);
+	IplImage *img2 = cvCreateImage(cvSize(4, 4), IPL_DEPTH_32F, 1);
+	IplImage *img3 = cvCreateImage(cvSize(4, 4), IPL_DEPTH_32F, 1);
+	void *data = malloc(img1->widthStep*img1->height);
+	klee_make_symbolic(data, img1->widthStep*img1->height, "img1data");
+	memcpy(img1->imageData, data, img1->widthStep*img1->height);
+	cvCornerMinEigenVal(img1, img2, 5);
+	cvUseOptimized(false);
+	cvCornerMinEigenVal(img1, img3, 5);
+	klee_print_expr("img2->imageData[0]", ((float *)img2->imageData)[0]);
+	klee_print_expr("img3->imageData[0]", ((float *)img3->imageData)[0]);
+}
+
