6813ca70c959617954414f9864d41df0a0b6c392
Peter Collingbourne <pcc03@doc.ic.ac.uk>
2010-06-30 17:10:51 +0100

    Fixes to run the benchmark

--- stereobm.cpp
+++ stereobm.cpp
@@ -6,15 +6,15 @@ int main(void) {
 	unsigned char mat1data[16];
 	unsigned char mat2data[16];
 	CvMat mat1, mat2;
-	CvMat *mat3v = cvCreateMat(4, 4, CV_16UC1);
-	CvMat *mat3s = cvCreateMat(4, 4, CV_16UC1);
+	CvMat *mat3v = cvCreateMat(16, 16, CV_16SC1);
+	CvMat *mat3s = cvCreateMat(16, 16, CV_16SC1);
 	CvStereoBMState *state;
 
 	klee_make_symbolic(mat1data, sizeof(mat1data), "mat1data");
 	klee_make_symbolic(mat2data, sizeof(mat2data), "mat2data");
 
-	mat1 = cvMat(4, 4, CV_8UC1, mat1data);
-	mat2 = cvMat(4, 4, CV_8UC1, mat2data);
+	mat1 = cvMat(16, 16, CV_8UC1, mat1data);
+	mat2 = cvMat(16, 16, CV_8UC1, mat2data);
 
 	cvUseOptimized(true);
 	state = cvCreateStereoBMState(0, 0);
@@ -25,7 +25,7 @@ int main(void) {
 	cvFindStereoCorrespondenceBM(&mat1, &mat2, mat3s, state);
 	cvReleaseStereoBMState(&state);
 
-	for (int i = 0; i < 1; i++) {
+	for (int i = 0; i < 256; i++) {
 		char buf[256];
 		sprintf(buf, "mat3s->data.s[%d]", i);
 		klee_print_expr(buf, mat3s->data.s[i]);
