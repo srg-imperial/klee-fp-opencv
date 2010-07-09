5edccc5b76c165b493c1d84940aae7207fa68735
Peter Collingbourne <pcc03@doc.ic.ac.uk>
2010-07-09 19:59:20 +0100

    Add command line arguments to stereobm

--- stereobm.cpp
+++ stereobm.cpp
@@ -4,51 +4,67 @@
 #endif
 #include <assert.h>
 #include <stdio.h>
-
-#include "get_seed.cpp"
+#include <unistd.h>
 
 int main(int argc, char** argv) {
-	unsigned char mat1data[4096];
-	unsigned char mat2data[4096];
+	unsigned char *mat1data;
+	unsigned char *mat2data;
 	CvMat mat1, mat2;
-	CvMat *mat3v = cvCreateMat(64, 64, CV_16SC1);
-	CvMat *mat3s = cvCreateMat(64, 64, CV_16SC1);
+	CvMat *mat3v, *mat3s;
 	CvStereoBMState *state;
 
+	int matwidth = 64, matheight = 64;
+	int disp = 32;
+	int do_random = 0;
+
+	int ch;
+	while ((ch = getopt(argc, argv, "w:h:d:r:")) != -1) {
+		switch (ch) {
+		case 'w': matwidth = atoi(optarg); break;
+		case 'h': matheight = atoi(optarg); break;
+		case 'd': disp = atoi(optarg); break;
 #ifdef __CONCRETE
-	int seed = get_seed(argc, argv);
-	srandom(seed);
-	
-	for (int i = 0; i < 4096; i++) {
+		case 'r': do_random = 1; srandom(atoi(optarg)); break;
+#endif
+		}
+	}
+
+	mat1data = (unsigned char *) malloc(matwidth*matheight);
+	mat2data = (unsigned char *) malloc(matwidth*matheight);
+
+#ifdef __CONCRETE
+	for (int i = 0; i < matwidth*matheight; i++) {
 	  mat1data[i] = random();
 	  mat2data[i] = random();
 	}
 #else
-	klee_make_symbolic(mat1data, sizeof(mat1data), "mat1data");
-	klee_make_symbolic(mat2data, sizeof(mat2data), "mat2data");
+	klee_make_symbolic(mat1data, matwidth*matheight, "mat1data");
+	klee_make_symbolic(mat2data, matwidth*matheight, "mat2data");
 #endif
 
-	mat1 = cvMat(64, 64, CV_8UC1, mat1data);
-	mat2 = cvMat(64, 64, CV_8UC1, mat2data);
+	mat1 = cvMat(matheight, matwidth, CV_8UC1, mat1data);
+	mat2 = cvMat(matheight, matwidth, CV_8UC1, mat2data);
+	mat3v = cvCreateMat(matheight, matwidth, CV_16SC1);
+	mat3s = cvCreateMat(matheight, matwidth, CV_16SC1);
 
 	cvUseOptimized(true);
 	state = cvCreateStereoBMState(0, 0);
-	state->roi1 = cvRect(0, 0, 63, 63);
-	state->roi2 = cvRect(0, 0, 63, 63);
-	state->numberOfDisparities = 32;
+	state->roi1 = cvRect(0, 0, matwidth-1, matheight-1);
+	state->roi2 = cvRect(0, 0, matwidth-1, matheight-1);
+	state->numberOfDisparities = disp;
 	cvFindStereoCorrespondenceBM(&mat1, &mat2, mat3v, state);
 	cvReleaseStereoBMState(&state);
 	cvUseOptimized(false);
 	state = cvCreateStereoBMState(0, 0);
-	state->roi1 = cvRect(0, 0, 63, 63);
-	state->roi2 = cvRect(0, 0, 63, 63);
-	state->numberOfDisparities = 32;
+	state->roi1 = cvRect(0, 0, matwidth-1, matheight-1);
+	state->roi2 = cvRect(0, 0, matwidth-1, matheight-1);
+	state->numberOfDisparities = disp;
 	cvFindStereoCorrespondenceBM(&mat1, &mat2, mat3s, state);
 	cvReleaseStereoBMState(&state);
 
 #ifdef __CONCRETE
 	int diffs = 0;
-	for (int i = 0; i < 1024; i++) {
+	for (int i = 0; i < matwidth*matheight; i++) {
 	  printf("Elem %d: %d vs %d", i, 
 		 mat3s->data.s[i], mat3v->data.s[i]);
 
@@ -63,7 +79,7 @@ int main(int argc, char** argv) {
 	  printf("Mismatches FOUND!\n");
 	else printf("No mismatches found.\n");
 #else
-	for (int i = 0; i < 1024; i++) {
+	for (int i = 0; i < matwidth*matheight; i++) {
 		char buf[256];
 		sprintf(buf, "mat3s->data.s[%d]", i);
 		klee_print_expr(buf, mat3s->data.s[i]);
